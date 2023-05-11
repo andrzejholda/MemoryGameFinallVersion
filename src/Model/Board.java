53c6e6e903769fe2ca80629f1735a7b47d7dc553
Andrzej <big.holda@gmail.com>
2023-05-11 20:25:09 +0200

    change way to collect cards

--- src/Model/Board.java
+++ src/Model/Board.java
@@ -1,60 +1,75 @@
 package Model;
 
-import View.GameInformations;
-
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Board extends Level {
 
-    public static String[][] basicBoard = new String[5][5];
-    public Card[][] cards = new Card[4][4];
+
+    private List<List<Card>> cards = new ArrayList<>();
     private List<String> wordsList = new ArrayList<>();
 
-   private boolean allCardsMatched = false;
+    private boolean allCardsMatched = false;
 
     public boolean isAllCardsMatched() {
         return allCardsMatched;
     }
 
-    public boolean checkIfMatched(Card[][] card) {
-
-        for (int row = 0; row < card.length; row++) {
-            for (int col = 0; col < card[row].length; col++) {
-                if (card[row][col].isMatched() == false) {
+    //checks if all cards are guessed
+    public boolean checkIfMatched() {
+        for (int row = 0; row < cards.size(); row++) {
+            for (int col = 0; col < cards.get(row).size(); col++) {
+                if (cards.get(row).get(col).isMatched() == false) {
                     return false;
                 }
             }
         }
-        allCardsMatched =true;
+        allCardsMatched = true;
         return true;
     }
-    //shuffle card - row and column
-    public void shuffleCards() {
+
+    //shuffle wordlist
+    public void shuffleWords() {
         Collections.shuffle(wordsList);
     }
-    //add
+
+    //add word to array
     public void addWordsToArray() {
-        int index = 0;
-        for (int row = 0; row < cards.length; row++) {
-            for (int column = 0; column < cards[0].length; column++) {
+        if (wordsList.isEmpty()) {
+            // return info
+            System.out.print("Pusto");
+            return;
+        }
+        int index = 1;
+        for (int row = 0; row < getRowUser(); row++) {
+            cards.add(new ArrayList<>());
+            for (int column = 0; column < getColumnUser(); column++) {
                 if (index == wordsList.size())
                     index = 0;
-                cards[row][column] = new Card(wordsList.get(index));
+                cards.get(row).add(new Card(wordsList.get(index)));
                 index++;
             }
         }
     }
+
+    //open the selected card
     public void openCard(int row, int column) {
-        cards[row -1][column -1].reveal(true);
+        cards.get(row - 1).get(column - 1).reveal(true);
     }
 
+    //close the selceted card
     public void closeCards(Card card1, Card card2) {
         card1.reveal(false);
         card2.reveal(false);
     }
 
+    //check match selected card
     public boolean checkMatch(Card card1, Card card2) {
-        if(card1.getValue().equals(card2.getValue())){
+        if (card1.getValue().equals(card2.getValue())) {
             card1.reveal(true);
             card1.setMatch(true);
             card2.reveal(true);
@@ -64,28 +79,23 @@ public class Board extends Level {
             return false;
     }
 
-    public void setWords(){
-        for(int i=0;i<2;i++) {
-            wordsList.add("KOT");
-            wordsList.add("BYK");
-            wordsList.add("JEŻ");
-            wordsList.add("SŁOŃ");
-            wordsList.add("MYSZ");
-            wordsList.add("SROKA");
-            wordsList.add("PIES");
-            wordsList.add("KURA");
-        }
+    //set words at list
+    public void setWords() {
+  //      int wordsAdd = 0;
+        int maxWords = (getRowUser() * getColumnUser() / 2);
+        chooseRangeOfLines = allLines.subList(0, maxWords);
+//        while (wordsAdd < maxWords) {
+//            monoWordsList.add(chooseRangeOfLines.get(wordsAdd));
+ //           wordsAdd++;
+//        }
+    }
 
+    public void evenList() {
+    for(int i=0;i<2;i++){
+        for(int a=0;a<chooseRangeOfLines.size();a++){
+            wordsList.add(chooseRangeOfLines.get(a));
         }
-
-//    //set value in object of card
-//    public void setCards() {
-//        this.cards = cards;
-//        cards[0]=new Card[]{new Card("KOT"),new Card("BYK"),new Card("JEŻ"),new Card("SŁOŃ")};
-//        cards[1]=new Card[]{new Card("MYSZ"),new Card("SROKA"),new Card("PIES"),new Card("KURA")};
-//        cards[2]=new Card[]{new Card("KOT"),new Card("BYK"),new Card("JEŻ"),new Card("SŁOŃ")};
-//        cards[3]=new Card[]{new Card("MYSZ"),new Card("SROKA"),new Card("PIES"),new Card("KURA")};
-//    }
-//
+    }
+    }
 
 }
