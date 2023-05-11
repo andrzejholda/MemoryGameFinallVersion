5b91189057605d065775d52f03ba696279059869
Andrzej <big.holda@gmail.com>
2023-05-11 20:29:36 +0200

    add exceptions if user give bad coordinates, show information about this,and try get correctly input.

--- src/GameController/Controller.java
+++ src/GameController/Controller.java
@@ -3,30 +3,77 @@ package GameController;
 import Model.Board;
 import View.GameInformations;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Controller {
     GameInformations gameInformations = new GameInformations();
     public static Scanner userChoice = new Scanner(System.in);
+    public static Scanner scanner = new Scanner(System.in);
     Board board = new Board();
 
+    public Controller() throws IOException {
+    }
+
     public void game() {
-        board.setWords();
-        board.addWordsToArray();
-        board.shuffleCards();
-        while (!board.checkIfMatched(board.cards)) {
-            gameInformations.printActuallyBoard( board);
-            gameInformations.firstCoordinates();
-            int row1 = userChoice.nextInt();
-            if (row1>board.cards.length || row1<0){
-                gameInformations.badCoordinat();
-                continue;
-            }
-            int column1 = userChoice.nextInt();
-            if (column1 >board.cards[row1-1].length || column1<0){
-                gameInformations.badCoordinat();
-                continue;
-            }
+        gameInformations.printMemoryGameTitle();
+        gameInformations.setBoardSize();
+        gameInformations.rowSet();
+        board.readIntInput(scanner,gameInformations.badChar,gameInformations.badCharInformation);
+        board.setRowUser(scanner.nextInt());
+        gameInformations.columnSet();
+        board.readIntInput(scanner,gameInformations.badChar,gameInformations.badCharInformation);
+        board.setColumnUser(scanner.nextInt());
+        while(!board.isBoardSizeCorrect()) {
+        if(board.getRowUser()<=0||board.getColumnUser()<=0){
+            gameInformations.wrongBoardSize();
+            gameInformations.setBoardSize();
+            gameInformations.rowSet();
+            board.readIntInput(scanner,gameInformations.badChar,gameInformations.badCharInformation);
+            board.setRowUser(scanner.nextInt());
+            gameInformations.columnSet();
+            board.readIntInput(scanner,gameInformations.badChar,gameInformations.badCharInformation);
+            board.setColumnUser(scanner.nextInt());
+        }else {
+            board.setBoardSizeCorrect(true);
+        }
+        }
+        while(board.getRowUser() * board.getColumnUser() % 2 != 0) {
+            gameInformations.oddBoardSize();
+            gameInformations.informationAboutEven();
+            gameInformations.rowSet();
+            board.readIntInput(scanner,gameInformations.badChar,gameInformations.badCharInformation);
+            board.setRowUser(scanner.nextInt());
+            gameInformations.columnSet();
+            board.readIntInput(scanner,gameInformations.badChar,gameInformations.badCharInformation);
+            board.setColumnUser(scanner.nextInt());
+        }
+            board.setWords();
+            board.evenList();
+            board.shuffleWords();
+            board.addWordsToArray();
+            gameInformations.printActuallyBoard(board);
+            while (!board.checkIfMatched()) {
+                gameInformations.firstCoordinates();
+                gameInformations.cardRow();
+                board.readIntInput(userChoice,gameInformations.badChar,gameInformations.badCharInformation);
+                int row1 = userChoice.nextInt();
+                if (row1 > board.getCardRow() || row1 <= 0) {
+                    gameInformations.badCoordinat();
+                    gameInformations.wrongRow();
+                    gameInformations.cardRow();
+                    board.readIntInput(userChoice,gameInformations.badChar,gameInformations.badCharInformation);
+                    row1 = userChoice.nextInt();
+                }
+                gameInformations.cardColumn();
+                board.readIntInput(userChoice,gameInformations.badChar,gameInformations.badCharInformation);
+                int column1 = userChoice.nextInt();
+                while (column1 > board.getCardsColumn(row1 - 1).size() || column1 <=0) {
+                    gameInformations.badCoordinat();
+                    gameInformations.wrongColumn();
+                    gameInformations.cardColumn();
+                    board.readIntInput(userChoice,gameInformations.badChar,gameInformations.badCharInformation);
+                    column1 = userChoice.nextInt();
 
             board.openCard(row1, column1);
             gameInformations.printActuallyBoard(board);
@@ -47,13 +94,7 @@ public class Controller {
         }
         gameInformations.gameEnd();
 
+
+        }
+
     }
-
-    //  board.setCards();
-//        board.setWords();
-//        board.shuffleCards();
-//        board.printActuallyBoard();
-    // gra trwa, dopóki wszystkie karty nie zostaną odkryte
-
-
-}
