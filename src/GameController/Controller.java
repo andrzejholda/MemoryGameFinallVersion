558acfed595e99731b5db09eab8b351bf7d86c8a
Andrzej <big.holda@gmail.com>
2023-05-11 20:30:40 +0200

    add exceptions if user give bad coordinates, show information about this,and try get correctly input.

--- src/GameController/Controller.java
+++ src/GameController/Controller.java
@@ -75,24 +75,59 @@ public class Controller {
                     board.readIntInput(userChoice,gameInformations.badChar,gameInformations.badCharInformation);
                     column1 = userChoice.nextInt();
 
-            board.openCard(row1, column1);
-            gameInformations.printActuallyBoard(board);
-            gameInformations.secondCoordinates();
-            int row2 = userChoice.nextInt();
-            int column2 = userChoice.nextInt();
-            board.openCard(row2, column2);
-            gameInformations.printActuallyBoard(board);
-            if (board.checkMatch(board.cards[row1 - 1][column1 - 1], board.cards[row2 - 1][column2 - 1])) {
-                gameInformations.correctMatches();
-                  if (board.isAllCardsMatched()) {
-                            gameInformations.finalInformations();
-                        }
-            } else {
-                gameInformations.wrongMatches();
-                board.closeCards(board.cards[row1 - 1][column1 - 1], board.cards[row2 - 1][column2 - 1]);
+                }
+                board.openCard(row1, column1);
+                gameInformations.printActuallyBoard(board);
+                gameInformations.secondCoordinates();
+                gameInformations.cardRow();
+                board.readIntInput(userChoice,gameInformations.badChar,gameInformations.badCharInformation);
+                int row2 = userChoice.nextInt();
+                if (row2 > board.getCardRow() || row2 <= 0) {
+                    gameInformations.badCoordinat();
+                    gameInformations.wrongRow();
+                    gameInformations.cardRow();
+                    board.readIntInput(userChoice,gameInformations.badChar,gameInformations.badCharInformation);
+                    row2 = userChoice.nextInt();
+                }
+                gameInformations.cardColumn();
+                board.readIntInput(userChoice,gameInformations.badChar,gameInformations.badCharInformation);
+                int column2 = userChoice.nextInt();
+                if (column2 > board.getCardsColumn(row2 - 1).size() || column2 <= 0) {
+                    gameInformations.badCoordinat();
+                    gameInformations.wrongColumn();
+                    gameInformations.cardColumn();
+                    board.readIntInput(userChoice,gameInformations.badChar,gameInformations.badCharInformation);
+                    column2 = userChoice.nextInt();
+                }
+                while (board.getCard(row1 - 1, column1 - 1) == board.getCard(row2 - 1, column2 - 1)) {
+                    gameInformations.sameCoordinat();
+                    gameInformations.secondCoordinates();
+                    gameInformations.cardRow();
+                    board.readIntInput(userChoice,gameInformations.badChar,gameInformations.badCharInformation);
+                    row2 = userChoice.nextInt();
+                    gameInformations.cardColumn();
+                    board.readIntInput(userChoice,gameInformations.badChar,gameInformations.badCharInformation);
+                    column2 = userChoice.nextInt();
+                }
+                board.openCard(row2, column2);
+                gameInformations.printActuallyBoard(board);
+                if (board.checkMatch(board.getCard(row1 - 1, column1 - 1), board.getCard(row2 - 1, column2 - 1))) {
+                    gameInformations.correctMatches();
+                    board.setMoves(board.increaseMoves());
+                    if (board.isAllCardsMatched()) {
+
+                        gameInformations.finalInformations();
+                    }
+                } else {
+                    board.setMoves(board.increaseMoves());
+                    gameInformations.wrongMatches();
+                    board.closeCards(board.getCard(row1 - 1, column1 - 1), board.getCard(row2 - 1, column2 - 1));
+                }
+
             }
-        }
-        gameInformations.gameEnd();
+
+            gameInformations.informationHowManyMoves(board.getMoves());
+            gameInformations.gameEnd();
 
 
         }
