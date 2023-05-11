86c3061d9d2e59aca44687e8c6774aa070597c73
Andrzej <big.holda@gmail.com>
2023-05-11 20:33:12 +0200

    change menu / start information and add whole information about program what user should to do.

--- src/View/GameInformations.java
+++ src/View/GameInformations.java
@@ -3,14 +3,30 @@ import Model.Board;
 
 public class GameInformations {
 
-
-
-    public void showLvl(int level){
-        System.out.print("Your actually level are: "+ level);
+    public String badChar="Podałeś nieodpowiedni znak, podaj liczbę !";
+    public String badCharInformation="Podaj jeszcze raz : ";
+    public  void printMemoryGameTitle() {
+        String[] colors = {"\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m"};
+        String[] letters = {"W","I","T","A","J"," ","W"," ","M", "E", "M", "O", "R", "Y", " ", "G", "A", "M", "E"};
+        int colorIndex = 0;
+        for (String letter : letters) {
+            System.out.print("\033[1m\033[3m" +colors[colorIndex % colors.length] + letter);// letter repeat incease size of letter
+            colorIndex++;
+            try {
+                Thread.sleep(200); // pauza for 100 milisekund
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("\033[0m"); // reset for basic colors
     }
+    public void setBoardSize(){System.out.println("Podaj rozmiar planszy do gry - jej rozmiar musi być parzysty");}
+    public void wronInput(){System.out.println("Podałeś nieodpowiedni znak, podaj liczbę !");}
+    public void rowSet(){System.out.println("Ilość kolum ? :");}
+    public void columnSet(){System.out.println("Ilość wierszy ? : ");}
+    public void wrongBoardSize(){System.out.println("Podałeś nieodpowiedni rozmiar planszy, spróbuj ponownie");}
+    public void printActuallyBoard(Board board){
 
-    public void printActuallyBoard( Board board){
-        showLvl(board.getLevel());
         System.out.println();
         for(int row=0;row<5;row++){
             for(int column=0;column<5;column++) {
